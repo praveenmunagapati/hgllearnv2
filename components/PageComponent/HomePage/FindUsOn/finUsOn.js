230bb3f112c138f5ea2f46b251bf93e06402fc48
Hari PD Banjade <hbanari93@outlook.com>
2022-08-13 03:09:00 +0545

    seo

--- components/PageComponent/HomePage/FindUsOn/finUsOn.js
+++ components/PageComponent/HomePage/FindUsOn/finUsOn.js
@@ -9,7 +9,7 @@ const FindUsOn = () => {
                 { Data.map((item, index) => {
                     return (
                         <div key={ index } className="flex flex-col items-center w-max shadow-[0px_0px_2px_0px_gray] px-5 rounded-md py-2 xxl:px-6 cursor-pointer hover:border-[1px] hover:border-black transition-all scale-105 duration-150">
-                            <div className="text-4xl xl:text-5xl xxl:text-6xl">{ item.icon }</div>
+                            <div className="text-4xl xl:text-5xl xxl:text-6xl"><a href="https://www.facebook.com/hubitts" target="_blank">{ item.icon }</a></div>
                             <div className="text-base xl:text-lg xxl:text-xl Poppins">500</div>
                             <div className="text-xs xl:text-sm xxl:text-base text-gray-500 capitalize">followers</div>
                         </div>
