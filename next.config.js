5c7d2250fae4c8606e1b06fb5678aed20db05b5b
Hari PD Banjade <hbanari93@outlook.com>
2022-08-20 16:49:33 +0545

    site maps

--- next.config.js
+++ next.config.js
@@ -3,6 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   siteUrl: process.env.SITE_URL || 'https://hubit.com.np',
   generateRobotsTxt: true,
+  generateIndexSitemap: false,
+  sitemapSize: 1000,
   images: {
     domains: ["hubitnep.herokuapp.com", "firebasestorage.googleapis.com"],
   },
