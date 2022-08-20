c3a4a6c86d731058745e80ea2054e8d81c95d357
Hari PD Banjade <hbanari93@outlook.com>
2022-08-20 13:37:15 +0545

    merger

--- pages/aboutUs/index.jsx
+++ pages/aboutUs/index.jsx
@@ -1,26 +1,20 @@
-import Head from "next/head";
 import React from "react";
 import MIssionVision from "../../components/PageComponent/AboutPage/MissionVision/MIssionVision";
 import WhatClientsSays from "../../components/PageComponent/AboutPage/What ClientsSays/WhatClientsSays";
 import WhatWeProvide from "../../components/PageComponent/AboutPage/whatWeProvideSection/WhatWeProvide";
 import WhoWeAre from "../../components/PageComponent/AboutPage/WhoWeAre/WhoWeAre";
 import Layout from "../../HOC/Layout/Layout";
-import styles from "../../styles/Home.module.css";
 
 function Aboutus() {
   return (
     <div>
-      <Head>
-
-      </Head>
-
       <Layout>
         <div>
           {/* <div className={styles.container}> */}
           <WhoWeAre />
           <MIssionVision />
           <WhatWeProvide />
-          <WhatClientsSays />
+          {/* <WhatClientsSays /> */}
           {/* </div> */}
         </div>
       </Layout>
