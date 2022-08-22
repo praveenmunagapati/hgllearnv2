e67a65456167a6becd46e973abdb3003ab24f28f
Ashish <asespandey2@gmail.com>
2022-08-22 13:44:19 +0545

    Add Placement

---
+++ components/PageComponent/Placement/BottomSection.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import { SiMicrosoft } from "react-icons/si";
+const BottomSection = () => {
+  return (
+    <>
+      <div className="p-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4  xxl:grid-cols-4  gap-5">
+        {[1, 2, 3, 4].map((item, index) => {
+          return (
+            <div
+              className="rounded-lg overflow-hidden shadow-lg group p-2 "
+              key={index}
+            >
+              <img
+                className="w-full group-hover:scale-105 transition-all duration-300"
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzXzIgxiHN9Ktcb5hqKCr_ezUySvhoawPrUQ&usqp=CAU"
+                alt="Mountain"
+                height={250}
+                width={250}
+              />
+              <div className="px-6 py-4">
+                <div className="font-bold text-xl mb-2">
+                  Placement At Hubit Pvt . Ltd
+                </div>
+              </div>
+              <hr />
+              <div className="px-6 pt-4 pb-2">
+                <span className="inline-block px-1 py-1 text-sm font-semibold mr-2 mb-2">
+                  Roshika Chettri
+                </span>
+                <span className=" px-1 py-1 text-sm font-semibold text-[#a0047d]  mb-2">
+                  Web Design
+                </span>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+      <div className="px-4 sm:pb-0 md:pb-3 lg:pb-12 xl:m-20 md:px-8 lg:px-20 xl:px-24 xxl:px-32 mt-20 flex flex-col items-center justify-center">
+        <div className="w-max text-[#a0047d] text-lg xl:text-xl xxl:text-2xl capitalize Poppins font-bold">
+          Our Placement Partners
+        </div>
+        <div className="w-full flex flex-row flex-wrap gap-10 justify-center my-6 pb-20">
+          {[1, 2, 3, 4, 5, 6].map((item) => {
+            return (
+              <div
+                key={item}
+                className="flex flex-row h-max items-center py-1 px-2 border border-gray-500 shadow-md "
+              >
+                <span className="text-base xl:text-lg xxl:text-xl">
+                  <SiMicrosoft />
+                </span>
+                <span className="ml-2 text-xs xl:text-sm xxl:text-base">
+                  Microsoft
+                </span>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default BottomSection;
---
+++ components/PageComponent/Placement/HeroSection.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import Image from "next/image";
+
+const HeroSection = () => {
+  return (
+    <>
+      <section className="">
+        <div className=" mx-auto flex px-5 py-24 md:flex-row lg:flex-row xl:flex-row xxl:flex-row flex-col items-center">
+          <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 xl:w-[30vw]  xxl:w-[50vw] flex flex-col md:items-start md:text-left mb-16 md:mb-0 ">
+            <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-main">
+              What we do for your placement?
+            </h1>
+            <p className="mb-8 leading-relaxed">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed quos
+              maxime dignissimos soluta beatae, explicabo quia aspernatur totam
+              quasi unde inventore architecto, dolore doloribus praesentium eos
+              ea labore sequi minus vel? Cumque quisquam vitae reiciendis
+              explicabo non error recusandae, dolorem nulla iusto laudantium,
+              voluptas animi consequatur ipsum deleniti quis aut.
+            </p>
+          </div>
+          <div className="xs:hidden sm:hidden lg:max-w-lg lg:w-full md:w-1/2 xl:w-3/6 xxl:w-3/6">
+            <Image
+              className="object-cover object-center rounded"
+              alt="Hubit"
+              src="https://www.derby-college.ac.uk/wp-content/uploads/2022/04/Courses-starting-april2022.jpg"
+              layout="responsive"
+              width={70}
+              height={40}
+            />
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default HeroSection;
