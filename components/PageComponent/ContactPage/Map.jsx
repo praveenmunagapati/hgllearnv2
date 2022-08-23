01f01cca0b2703ccdb27092067e462bfc3007838
haripdbanjade <haripdbanjade@gmail.com>
2022-08-23 22:39:57 +0545

    content edit for contat us page

--- components/PageComponent/ContactPage/Map.jsx
+++ components/PageComponent/ContactPage/Map.jsx
@@ -6,8 +6,7 @@ const Map = () => {
       <div className="flex flex-col items-center justify-center gap-0">
         <h1 className="text-main font-bold h-[36px]">Find us on Google Maps</h1>
         <p className="font-medium mb-4 ">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, eaque
-          Lorem ipsum dolor sit amet.
+          You can also find us on google map by using this map
         </p>
         <div className="relative text-right xs:w-[400px] sm:w-[600px]  h-[400px] md:w-[700px] lg:w-[900px]xl:w-[1000px] xxl:w-[1100px] mb-20">
           <div className="overflow-hidden bg-none h-[400px]  xs:w-[400px] sm:w-[600px]   md:w-[700px] lg:w-[900px]xl:w-[1000px] xxl:w-[1100px]">
