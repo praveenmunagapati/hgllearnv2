ba55dd7935e1ee87ca67e0e872bb41e8b00ad078
nischal karki <nischalkarki@nischals-MacBook-Pro.local>
2022-08-24 09:32:12 +0545

    mainpush

--- pages/index.jsx
+++ pages/index.jsx
@@ -8,8 +8,13 @@ export default function Index() {
       <div className={``}>
         {/* <div> */}
         <Head>
-          <title>HUB IT GROUP PVT LTD. - Providing IT Training,Solution,Digital Marketing ,Hardware/Networking</title>
-          <meta name="description" content="Best IT training and Solutions in Butwal, No.1 web designer and computer Institute in Rupandehi, Best IT Training in Butwal Nepal
+          <title>
+            HUB IT GROUP PVT LTD. - Providing IT Training,Solution,Digital
+            Marketing ,Hardware/Networking
+          </title>
+          <meta
+            name="description"
+            content="Best IT training and Solutions in Butwal, No.1 web designer and computer Institute in Rupandehi, Best IT Training in Butwal Nepal
   | A Top Computer Institute in Rupandehi |No.1 IT Center | Best website designing course in Butwal | Top IT Training institute in Butwal |
   No.1 Computer training in Nepal | IT training in Butwal | IT training Nepal | HUB IT TRAINING AND SOLUTION |hub it training and solution |
   Computer Training Institute in kathmandu Nepal, Professional IT Training institute in Nepal, computer learning center , computer training center in nepal,
@@ -17,8 +22,11 @@ export default function Index() {
   Computer Programming Institute Nepal, Professional Web Designing , Professional Web Development, Advance Web Master Program, Professional JOOMLA (CMS) Development,
   CSS Training Nepal, Object Oriented PHP Programming Training Courses Nepal, Quality Assurance, Web Institution Nepal, Computer courses in nepal,
   Search Engine Optimization Training Nepal, SEO Courses, C , C++ , VB.Net , ASP.Net , C# , Oracle , MS SQL Server , PHP MYSQL ,
-  Java, JavaBeans , JSP , J2EE training In kathmandu nepal. BUtwal" />
-<meta name="keywords" content="Best IT Training In Butwal,best it training in nepal,tech news,tech,new technology,
+  Java, JavaBeans , JSP , J2EE training In kathmandu nepal. BUtwal"
+          />
+          <meta
+            name="keywords"
+            content="Best IT Training In Butwal,best it training in nepal,tech news,tech,new technology,
   what is technology,technology definition,science and technology,latest technology,technews,latest technology news,cool tech,technological advances
 ,technology review,newtech,modern technology,definition of technology,best it training in rupandehi,best it training lumbini,best computer institute in nepal,best computer institute in butwal,best computer institute in rupandehi,
   best computer institute in lumbini,No,1 it training in butwal,No,1 it training in Nepal,No,1 it training in Butal,No,1 it training in lumbini,No,1 it training in rupandehi,
@@ -44,22 +52,32 @@ export default function Index() {
   mobile application development companies in nepal,android app development company,webdesign,college in butwal,website design,event videography,  Best IT Training in nepal,
   it training courses,it training near me,it training butwal,training course,computer training in butwal,best it training website,best it training courses, best it training sites,
   best it training in the world,best computer institute in kathmandu,best computer institute in chitwan,best computer training institute in butwal, website designing course,website designing course in butwal,
-  it training institute in nepal," />
-<meta property="og:title" content="BEST IT TRAINING IN BUTWAL NEPAL _ AT Top Computer Institute in Rupandehi |No.1 IT Center |
+  it training institute in nepal,"
+          />
+          <meta
+            property="og:title"
+            content="BEST IT TRAINING IN BUTWAL NEPAL _ AT Top Computer Institute in Rupandehi |No.1 IT Center |
   Best website designing course in Butwal | Top IT Training institute in Butwal | No.1 Computer training in Nepal | IT training in Butwal |
   IT training Nepal | Mirror Technology and Training Center | best video editing and wordpress training in butwal |
   HTML, CSS, Java, Python, PHP, Javascript, Wordpress, Django, Android | WEB DESIGN IN BUTWAL | WEBSITE DESIGN IN BUTWAL NEPAL |
-  WEB DEVELOPER IN BUTWAL | WEBSITE DESIGN |WEBDESIGN | HTML COURSE IN BUTWAL NEPAL | COMPUTER TRAINING IN BUTWAL | WORDPRESS IN BUTWAL NEPAL| WEBSITE COURSES IN BUTWAL NEPAL" />
-<meta property="og:description" content="Best IT training and Solutions in Butwal, No.1 web designer and computer Institute in Rupandehi, Best IT Training in Butwal Nepal |
+  WEB DEVELOPER IN BUTWAL | WEBSITE DESIGN |WEBDESIGN | HTML COURSE IN BUTWAL NEPAL | COMPUTER TRAINING IN BUTWAL | WORDPRESS IN BUTWAL NEPAL| WEBSITE COURSES IN BUTWAL NEPAL"
+          />
+          <meta
+            property="og:description"
+            content="Best IT training and Solutions in Butwal, No.1 web designer and computer Institute in Rupandehi, Best IT Training in Butwal Nepal |
   A Top Computer Institute in Rupandehi |No.1 IT Center | Best website designing course in Butwal | Top IT Training institute in Butwal | No.1 Computer training in Nepal |
   IT training in Butwal | IT training Nepal | Mirror Technology and Training Center | Computer Training Institute in kathmandu Nepal, Professional IT Training institute in Nepal,
   computer learning center , computer training center in nepal, computer courses in nepal ,computer institute in nepal, Web Designing Training,Professional IT Education, Training,
   IT Education Nepal, Computer Traning Institute Nepal, Computer Programming Institute Nepal, Professional Web Designing , Professional Web Development,
   Advance Web Master Program, Professional JOOMLA (CMS) Development, CSS Training Nepal, Object Oriented PHP Programming Training Courses Nepal, Quality Assurance,
   Web Institution Nepal, Computer courses in nepal,  Search Engine Optimization Training Nepal, SEO Courses, C, C++ , VB.Net , ASP.Net , C# , Oracle , MS SQL Server ,
-  PHP MYSQL , Java  , JavaBeans , JSP , J2EE training In kathmandu nepal. BUtwal" />
+  PHP MYSQL , Java  , JavaBeans , JSP , J2EE training In kathmandu nepal. BUtwal"
+          />
           <link rel="icon" href="/hubitLogo.svg" />
-          <meta name="msvalidate.01" content="DCD5FD5B8698F83AE6AC76EE889570AC" />
+          <meta
+            name="msvalidate.01"
+            content="DCD5FD5B8698F83AE6AC76EE889570AC"
+          />
         </Head>
         <main>
           <Home />
