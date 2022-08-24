1a3a57a44524609f03377329f47fd9ef63d64cfa
haripdbanjade <haripdbanjade@gmail.com>
2022-08-24 23:54:20 +0545

    home ma why to choose us add gareko

--- components/PageComponent/HomePage/WhyHub/whyHub.js
+++ components/PageComponent/HomePage/WhyHub/whyHub.js
@@ -1,6 +1,37 @@
 import { GiTeacher } from "react-icons/gi";
 const WhyHub = () => {
-    let data = [1, 2, 3, 4, 5, 6];
+    let data = [
+        {
+            icon:<GiTeacher />,
+            title:'Well-Equipped Lab',
+            description:'Labs are equipped with world class equipments and facilities based on the latest technology pertaining to different sectors. The students and faculties push the boundaries of possibilities in these world class labs, and there is always a constant buzz in this part of the institute where new knowledge is created.'
+        },
+        {
+            icon:<GiTeacher />,
+            title:'Highly experience instructor',
+            description:'Our Instructors are highly qualified active professional working in an organization as developer,designer,technician with years of expertise in their own field. They are continuously conducting trainings in our advanced training centre using a cutting-edge technology.'
+        },
+        {
+            icon:<GiTeacher />,
+            title:'Updated Syllablus',
+            description:'Designed syllablus focusing the sector as need of industries demand in which our students can compete the requirement need.We also research new technology or tools which will be required on the industries in present or future and we add the content to our Syllablus. '
+        },
+        {
+            icon:<GiTeacher />,
+            title:'Personality Development training',
+            description:''
+        },
+        {
+            icon:<GiTeacher />,
+            title:'Well-Equipped Lab',
+            description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. In sunt optio unde rem nulla, quibusdam aperiam,vol'
+        },
+        {
+            icon:<GiTeacher />,
+            title:'Well-Equipped Lab',
+            description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. In sunt optio unde rem nulla, quibusdam aperiam,vol'
+        }
+    ];
 
     return (
         <div className="px-4 md:px-8 lg:px-20 xl:px-24 xxl:px-32 w-full h-max my-16">
@@ -15,9 +46,14 @@ const WhyHub = () => {
                         return (
                             <div key={ item }>
                                 <div className="flex flex-col items-center">
-                                    <div className="p-2 rounded-full border border-gray-700 text-sm xl:text-base xxl:text-lg"><GiTeacher className="" /></div>
-                                    <div className="text-xs xl:text-sm xxl:text-base font-bold w-full mt-3 mb-1 Poppins">Well-Equipped Lab</div>
-                                    <div className="text-justify text-xs xl:text-sm xxl:text-base text-gray-700">Lorem ipsum dolor sit amet consectetur adipisicing elit. In sunt optio unde rem nulla, quibusdam aperiam,vol</div>
+                                    <div className="p-2 rounded-full border border-gray-700 text-sm xl:text-base xxl:text-lg">
+                                       {item.icon} </div>
+                                    <div className="text-xs xl:text-sm xxl:text-base font-bold w-full mt-3 mb-1 Poppins">
+                                    {item.title}
+                                    </div>
+                                    <div className="text-justify text-xs xl:text-sm xxl:text-base text-gray-700">
+                                    {item.description}
+                                    </div>
                                 </div>
                             </div>
                         );
